17adc408249794ebe4de91d87ee7e18108b36847
xszxc <2112067692@qq.com>
2023-09-14 13:57:11 +0800

    feat:增加一些通用模块

---
+++ common/projectCode.go
@@ -0,0 +1,3 @@
+package common
+
+/*业务模块错误代码*/
---
+++ common/userCode.go
@@ -0,0 +1,15 @@
+package common
+
+/*用户模块错误代码*/
+
+var (
+	CacheError             = NewError(500001, "Cache错误")
+	DBError                = NewError(500002, "DB错误")
+	NoLegalMobile          = NewError(100001, "手机号不合法")
+	CaptchaError           = NewError(100002, "验证码不合法")
+	CaptchaNotExist        = NewError(100003, "验证码不存在")
+	AccountExist           = NewError(100004, "账号已被注册")
+	MobileExist            = NewError(100005, "手机号已被注册")
+	AccountOrPasswordError = NewError(100006, "用户或密码错误")
+	NoLogin                = NewError(100007, "未登录")
+)
