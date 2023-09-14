7bcd0ae379a5cc71be95ec613a6ef6882fb06fd7
xszxc <2112067692@qq.com>
2023-09-14 16:30:19 +0800

    style：修改注释

--- api/router.go
+++ api/router.go
@@ -4,20 +4,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// 路由注册方法接口
+// Router 路由注册方法接口
 type Router interface {
 	Route(r *gin.Engine)
 }
 
-// RegisterRouter
 type RegisterRouter struct {
 }
 
 var routers []Router
 
-// 批量注册路由
+// InitRouter 批量注册路由
 func InitRouter(r *gin.Engine) {
-
 	for _, ro := range routers {
 		ro.Route(r)
 	}
