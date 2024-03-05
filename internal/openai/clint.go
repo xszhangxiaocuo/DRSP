576c9fec1518d37ac402f289b4ba78cf18cb07ba
xszxc <2112067692@qq.com>
2024-03-05 20:13:43 +0800

    feat:使用langchaingo调用第三方gpt接口

--- internal/openai/clint.go
+++ internal/openai/clint.go
@@ -1,6 +1,6 @@
 package openai
 
-var Gpt = NewGPT()
+var Gpt = NewGpt()
 
 //TODO:每个新的会话都要一个新的gpt实例
 
---
+++ internal/openai/openai_default.go
@@ -0,0 +1,52 @@
+package openai
+
+import (
+	"DRSP/config"
+	"context"
+	"fmt"
+	"github.com/sashabaranov/go-openai"
+)
+
+type DefaultGPT struct {
+	Client   *openai.Client
+	messages []openai.ChatCompletionMessage
+}
+
+func NewGPT() *DefaultGPT {
+	return &DefaultGPT{
+		Client: openai.NewClient(config.AppConf.OpenaiConfig.Key),
+	}
+}
+
+func (gpt *DefaultGPT) DefaultChat(question string) string {
+	var text string
+	if len(gpt.messages) == 0 {
+		text = fmt.Sprintf("我将提供一段由ocr识别生成的食品信息，由于ocr识别可能存在错误，分析过程中请你请你注意配料以及过敏原的合理性，接下来根据以下文本提取出食品的配料并分析配料中可能存在的过敏原，如果有危害人体健康对配料也请指出：%s", question)
+	} else {
+		text = question
+	}
+	gpt.messages = append(gpt.messages, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleUser,
+		Content: text,
+	})
+
+	resp, err := gpt.Client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model:    openai.GPT3Dot5Turbo,
+			Messages: gpt.messages,
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return err.Error()
+	}
+
+	content := resp.Choices[0].Message.Content
+	gpt.messages = append(gpt.messages, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleAssistant,
+		Content: content,
+	})
+	return content
+}
